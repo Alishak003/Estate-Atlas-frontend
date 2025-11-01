72ae2c01196cf8b2af707c5e72633070c3248832
Alisha <Alishakhanstack@gmail.com>
2025-11-01 11:28:12 +0530

    deleted file

--- src/app/dashboard/CompetitionFeedback/page.tsx
+++
@@ -1,9 +0,0 @@
-import { FeedbackForm } from "@/components/dashboard/Account/feedback-form"
-interface ChildComponentProps {
-    handleBack : (stepValue:string)=>void
-}
-export default function CompetitionFeedback({handleBack}:ChildComponentProps){
-    return(
-        <FeedbackForm handleBack={handleBack}/>
-    )
-}
